c2105e056335df3d258e3f4be5aa5be768555f0c
Rogerio Pereira <hi@rogeriopereira.dev>
2024-12-23 13:33:32 -0500

    feat: Middleware to fix status codes

---
+++ src/Middleware/ApiResponseFixerMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace RogerioPereira\ApiStatusFixer\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiResponseFixerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //Run all chain of middlewares before fixing the return
+        $response = $next($request);
+
+        $config = config('api-status-fixer.fix_status_ranges');
+
+        $status = $response->getStatusCode();
+
+        if (
+            ($config['20x'] == true && $status >= 200 && $status <= 299) ||
+            ($config['30x'] == true && $status >= 300 && $status <= 399) ||
+            ($config['40x'] == true && $status >= 400 && $status <= 499) ||
+            ($config['50x'] == true && $status >= 500 && $status <= 599)
+        ) {
+            //Define response Data
+            $responseData = [
+                ...$response->original,     //Add all original response items to new response, keeping their keys
+                'httpStatus' => $status,
+                // 'httpMessage' => $response->getStatusText(),
+            ];
+
+            return response()->json($responseData, 200);
+        }
+
+        return $response;
+    }
+}
