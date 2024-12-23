9e049650a6d06be417dcb47660e93a3e40a63348
Rogerio Pereira <hi@rogeriopereira.dev>
2024-12-23 13:52:27 -0500

    feat: Include Status name on response payload

--- src/Middleware/ApiResponseFixerMiddleware.php
+++ src/Middleware/ApiResponseFixerMiddleware.php
@@ -28,16 +28,29 @@ class ApiResponseFixerMiddleware
             ($config['40x'] == true && $status >= 400 && $status <= 499) ||
             ($config['50x'] == true && $status >= 500 && $status <= 599)
         ) {
+            $statusText = $this->getStatusName($status);
+
             //Define response Data
             $responseData = [
                 ...$response->original,     //Add all original response items to new response, keeping their keys
-                'httpStatus' => $status,
-                // 'httpMessage' => $response->getStatusText(),
+                'httpStatusCode' => $status,
+                'httpStatus' => $statusText,
             ];
 
             return response()->json($responseData, 200);
         }
 
         return $response;
+    }
+
+    /**
+     * Return HTTP Status name based on their cUndocumented functionode
+     *
+     * @param integer $statusCode
+     * @return string
+     */
+    private function getStatusName(int $statusCode): string
+    {
+        return Response::$statusTexts[$statusCode] ?? 'Unknown Status';
     }
 }
